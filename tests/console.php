f9e7e0f995a6c71a3b1ed76a199d2f2d418b23f1
AntinDehoda <dehoda@ukr.net>
2019-04-22 22:10:46 +0300

    The logic of tests with new classes changes

--- tests/console.php
+++ tests/console.php
@@ -2,20 +2,20 @@
 
 
 <?php
-require_once __DIR__ . '/../src/ObjectToArray.php';
-require_once __DIR__ . '/../src/outputs/JsonParser.php';
-require_once __DIR__ . '/../src/outputs/YamlParser.php';
-require_once __DIR__ . '/../src/outputs/XMLParser.php';
+require_once __DIR__ . '/../src/ObjectParser.php';
 
 $obj01 = "String object";
 $obj02 = 15;
 $obj03 = [$obj01, $obj02];
 $obj04 = (object) array('1' => 'foo', (object) array('2' => 'innerFoo'), 'bool' => true);
+$obj05 = true;
 
-
-$analizer01 = new Object_toArray();
-$output01 = $analizer01->getObject($obj04);
-$json01 =  new JsonParser();
+$analizer01 = new Object_toArray($obj05, 'Yaml');
+//$output01 = $analizer01->parseObject($obj04);
+//$json01 =  new JsonParser();
 //echo $json01->convertArray($output01);
-$yaml01 = new YamlParser();
-//echo $yaml01->convertArray($output01);
+//$yaml01 = new YamlParser();
+//$xml01 = new XMLParser();
+//
+//echo $xml01->convertArray($output01);
+echo $analizer01->convertOtput();
