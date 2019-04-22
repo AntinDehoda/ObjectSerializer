07d2a3716365648c14b87fd9952c4f8c98abafca
AntinDehoda <dehoda@ukr.net>
2019-04-22 22:07:42 +0300

    Renames ObjectToArray.php to ObjectParser.php and change some logic

---
+++ src/ObjectParser.php
@@ -0,0 +1,47 @@
+<?php
+require_once __DIR__ . '/../src/outputs/JsonParser.php';
+require_once __DIR__ . '/../src/outputs/YamlParser.php';
+require_once __DIR__ . '/../src/outputs/XMLParser.php';
+
+class Object_toArray
+{
+    private $obj_props = [];
+
+    public $output_type;
+
+    public function __construct($obj, $output_type = 'Json')
+    {
+        $this->output_type = $output_type . 'Parser';
+        $this->parseObject($obj);
+    }
+    public function parseObject($obj)
+    {
+        if (is_scalar($obj)) {
+            $this->obj_props[] = $obj;
+            return true;
+        } elseif ( !isset($obj)) {
+            $this->obj_props[] = 'Undefined or Null';
+            return false;
+        }
+        $public_properties = (is_array($obj)) ? $obj : get_object_vars($obj);
+        foreach ($public_properties as $prop => $value) {
+            if (is_null($value)) {
+                $this->obj_props[$prop] = 'NULL';
+            } elseif (!isset($value)) {
+                $this->obj_props[$prop] = 'Undefined';
+            } elseif (is_scalar($value)) {
+                $this->obj_props[$prop] = $value;
+            } else {
+                $this->obj_props[$prop] = $this->parseObject($value);
+            }
+        }
+
+        return $this->obj_props;
+    }
+    public function convertOtput()
+    {
+        $ouput = new $this->output_type();
+        return $ouput->convertArray($this->obj_props);
+    }
+
+}
